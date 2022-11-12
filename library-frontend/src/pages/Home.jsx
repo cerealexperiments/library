5182e6b66876eebaa7209025a080f5e76c98b638
rin <prettysunshine468@gmail.com>
2022-11-12 20:24:57 +0600

    add content to home page

--- library-frontend/src/pages/Home.jsx
+++ library-frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { motion } from "framer-motion";
 
-function Home(props) {
+function Home() {
   return (
-    <div>
-      <h2 className="font-semibold text-xl">Home</h2>
-    </div>
+    <motion.div
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+      className="hero min-h-[750px]"
+      style={{ backgroundImage: `url("https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg")` }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
+          <p className="mb-5">Please sign up to get access to all features of our library</p>
+          <button className="btn btn-outline bg-white border-white hover:bg-white hover:text-black hover:border-white">Get Started</button>
+        </div>
+      </div>
+    </motion.div>
   );
 }
 
