c1169b38533ea27e9b05e92aa1d46dda94728b31
rin <prettysunshine468@gmail.com>
2022-11-12 20:26:38 +0600

    update root component

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header.jsx";
 import {Routes, Route} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home.jsx";
@@ -7,6 +6,7 @@ import About from "./pages/About.jsx";
 import Books from "./pages/Books.jsx";
 import BookPage from "./pages/BookPage.jsx";
 import {BooksProvider} from "./context/BooksContext.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   const client = new QueryClient({});
