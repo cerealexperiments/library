4ed9a0754283972027d7f82d6b767015aaa1fbf0
rin <prettysunshine468@gmail.com>
2022-11-12 20:26:10 +0600

    add responsiveness to BooksList component

--- library-frontend/src/components/BooksList.jsx
+++ library-frontend/src/components/BooksList.jsx
@@ -3,7 +3,7 @@ import Book from "./Book.jsx";
 
 function BooksList({ books }) {
   return (
-    <div className="flex gap-8 flex-wrap">
+    <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2 md:grid-cols-3 gap-8 ">
       {books.map(book => <Book key={book.id} book={book}/>)}
     </div>
   );
